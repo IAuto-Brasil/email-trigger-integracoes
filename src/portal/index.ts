11d5f9661daa8d0fadca4c3cf530400c13b18c44
tiago-dias-laureano <tiagodiaslaureano32@gmail.com>
2025-08-29 19:11:22 -0300

    create new branch to news features

--- src/portal/index.ts
+++ src/portal/index.ts
@@ -1,12 +1,7 @@
-import { ParsedEmail } from "../services/emailMonitor";
 import processEmailWithGPT from "./processEmailWithGPT";
-// import processChavesNaMao from "./chavesnamao";
-// import processIcarros from "./icarros";
-// import processMobiauto from "./mobiauto";
-// import processSocarrao from "./socarrao";
-// import processUsadosBr from "./usadosbr";
 
 export default function processEmail(email: any) {
+  // gerenciar com RabbitMQ
   try {
     return processEmailWithGPT(email);
   } catch (error) {
